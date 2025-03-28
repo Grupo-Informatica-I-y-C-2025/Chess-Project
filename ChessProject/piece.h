02f2a9df43e3fb2cbc52329e54a8c07d95edd6eb
Remi-Garcia <remi.garcia@alumnos.upm.es>
2025-03-28 22:49:11 +0100

    Update piece.h
    añadidas las funciones de piece.cpp eliminado, y creación de una nueva enumeración para implementar los movimientos de peon en passant.

--- ChessProject/piece.h
+++ ChessProject/piece.h
@@ -6,14 +6,17 @@
 
 using namespace std;
 
+
 class Object {
 	//interface class for all pieces
 public:
-	enum type_t { PAWN,ROOK,BISHOP, KNIGTH,QUEEN,KING, EMPTY_CELL };
-	enum color_t {WHITE,BLACK,NONE};
+	enum flag_t { WHITE_EP, BLACK_EP, NO_FLAG };
+	enum type_t { PAWN, ROOK, BISHOP, KNIGHT, QUEEN, KING, EMPTY_CELL };
+	enum color_t { WHITE, BLACK, NONE };
 	virtual void getCellNumber(int& x, int& y) = 0;
 	virtual type_t getType() = 0;
 };
+
 
 class Piece :public Object {
 public:
@@ -23,15 +26,18 @@ protected:
 	int y;
 	type_t type;
 	color_t color;
+	flag_t flag;
 public:
 	bool select = 0;
-	Piece() :x(-1), y(-1), type(EMPTY_CELL), color (NONE) {}
+	Piece() :x(-1), y(-1), type(EMPTY_CELL), color (NONE),flag(NO_FLAG) {}
 	void setCell(int x, int y, type_t t, color_t col) { this->x = x; this->y = y; this->type = t; this->color = col; }
+	void setFlag(int x, int y, flag_t f) { this->x = x; this->y = y; this->flag = f; }
 	virtual void getCellNumber(int& x, int& y) { x = this->x; y = this->y; }
 	virtual type_t getType() { return type; }
 	virtual color_t getColor() { return color; }
-	virtual void selectCell();
-	virtual void unselectCell();
+	virtual flag_t getFlag() { return flag; }
+	virtual void selectCell(){ select = 1; }
+	virtual void unselectCell() {select = 0;}
 	virtual bool selected() { return select; }
 
 };
