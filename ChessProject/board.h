dd230ccbfbe88206effa80acfb54a4e373aa0d64
Remi-Garcia <remi.garcia@alumnos.upm.es>
2025-04-01 12:58:23 +0200

    Update board.h
    externalización de la función setboard, declaración de varias funciónes en relación al bot de movimientos de la maquina.

--- ChessProject/board.h
+++ ChessProject/board.h
@@ -10,45 +10,18 @@ using namespace std;
 class Board {
 
 protected:
+	
 	const int N;		//NxN board
 	Piece** tab;
 
 public:
-	Board(int n) :N(n) {
+	Board(int n, bool i) :N(n), Kramnik(i) {
 
 		tab = new Piece * [N];
 		for (int i = 0; i < N; i++) {
 			tab[i] = new Piece[N];			//initially empty
 		}
-		//set initial position
-		for (int j = 0; j < N; j++) {
-			tab[1][j].setCell(1, j, Piece::PAWN, Piece::BLACK);
-		}
-		for (int j = 0; j < N; j++) {
-			tab[N - 2][j].setCell(N-2, j, Piece::PAWN, Piece::WHITE);
-		}
-		tab[0][0].setCell(0, 0, Piece::ROOK, Piece::BLACK);
-		tab[0][7].setCell(0, 7, Piece::ROOK, Piece::BLACK);
-		tab[7][0].setCell(7, 0, Piece::ROOK, Piece::WHITE);
-		tab[7][7].setCell(7, 7, Piece::ROOK, Piece::WHITE);
-
-		tab[0][2].setCell(0, 2, Piece::BISHOP, Piece::BLACK);
-		tab[0][5].setCell(0, 5, Piece::BISHOP, Piece::BLACK);
-		tab[7][2].setCell(7, 2, Piece::BISHOP, Piece::WHITE);
-		tab[7][5].setCell(7, 5, Piece::BISHOP, Piece::WHITE);
-
-		tab[0][1].setCell(0, 1, Piece::KNIGHT, Piece::BLACK);
-		tab[0][6].setCell(0, 6, Piece::KNIGHT, Piece::BLACK);
-		tab[7][1].setCell(7, 1, Piece::KNIGHT, Piece::WHITE);
-		tab[7][6].setCell(7, 6, Piece::KNIGHT, Piece::WHITE);
-
-		tab[7][3].setCell(7, 3, Piece::QUEEN, Piece::WHITE);
-		tab[0][3].setCell(0, 3, Piece::QUEEN, Piece::BLACK);
-
-		tab[7][4].setCell(7, 4, Piece::KING, Piece::WHITE);
-		tab[0][4].setCell(0, 4, Piece::KING, Piece::BLACK);
-
-
+	
 	}
 
 	~Board() {
@@ -59,23 +32,39 @@ public:
 
 	int getSize() { return N; }
 	Piece** getTab() { return tab; }
+	Object::color_t getTurn() { return turn; }
 
 	//reglas de movimiento
 	void activate(int xcell_sel, int ycell_sel);
 	void registerCall(int xcell_sel, int ycell_sel);
 	bool makeMove();
-	bool scanCheckMate();
+	bool scanCheckMate(Object::color_t turn);
+	void setBoard(/*game_type*/);
+
+
+	//funciones para implementar bot
+	//si la partida es con bot
+	Object::color_t player = Object::WHITE;
+	Object::color_t bot = Object::BLACK;
+
+	int piecePunctuation(Object::type_t);
+	int EvaluateGame();
+	int minimax(int depth,int*,int*, bool maximizingPlayer);
+
+
 
 	private:
+	const bool Kramnik;
 	bool action;
 	int src[2] = { -1,-1 }, dest[2] = { -1,-1 };
 	int ky, kx;
 	Object::color_t turn = Object::WHITE;//turno inicial
 
+	bool isKramnik() { return Kramnik; }
 	void doMove();
 	void unselectAll();
 	void changeTurn();
-	void findKing(Object::color_t c);
+	bool findKing(Object::color_t c);
 
 	bool scanChecks(Object::color_t c);
 
