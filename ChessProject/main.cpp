1522afe21eab7f1dc4a17c30d9f3a851a07519d8
Remi-Garcia <remi.garcia@alumnos.upm.es>
2025-04-05 13:11:46 +0200

    Update main.cpp
    implementación de la funcion ontimer que refresca constantemente la generación de movimiento

--- ChessProject/main.cpp
+++ ChessProject/main.cpp
@@ -6,25 +6,33 @@
 #include "freeglut.h"
 #include <iostream>
 #include "boardgl.h"
+#pragma once
 
 
-#define GRID_SIZE	8		//NxN grid
+#define GRID_SIZEY	8	
+#define GRID_SIZEX  8	//NxN grid
+#define KRAMNIK 0
 
 /////////////////////////////////
 //call back declarations: will be called by glut when registered
 void OnDraw(void);
 void OnKeyboardDown(unsigned char key, int x, int y);
 void OnMouseClick(int button, int state, int x, int y);
-//void OnTimer(int value); 
+void OnTimer(int value); 
+//void motion(int x, int y);
 
 ////////////////////////////////////////////////
 //global objects which make the world
-Board gameboard(GRID_SIZE);
-BoardGL scene(&gameboard);
+Board gameboard(GRID_SIZEY,GRID_SIZEX, KRAMNIK);
+Game game(&gameboard);
+BoardGL scene(&gameboard,game);
+
+
 
 ///////////////////////////////////////////////
 
 int main(int argc, char* argv[]) {
+
 	//GL Initialization stuff
 	glutInit(&argc, argv);
 	glutInitWindowSize(800, 600);
@@ -32,12 +40,13 @@ int main(int argc, char* argv[]) {
 	glutCreateWindow("MiJuego");
 
 
-	//Register callbacks
+
+	glutTimerFunc(25,OnTimer,0);			 //set timer if required, currently not used
 	glutDisplayFunc(OnDraw);
 	glutKeyboardFunc(OnKeyboardDown);
 	glutMouseFunc(OnMouseClick);
-	//glutTimerFunc(25,OnTimer,0);			 //set timer if required, currently not used
-
+	//glutMotionFunc(motion);
+	
 	//sets light and perspective
 	scene.init();
 
@@ -82,17 +91,53 @@ void OnMouseClick(int b, int state, int x, int y) {
 	bool sKey = specialKey & GLUT_ACTIVE_SHIFT;
 
 
+
+	//
 	scene.MouseButton(x, y, b, down, sKey, ctrlKey);
 	
 
 	glutPostRedisplay();
 }
 
+/*void motion(int x, int y) {
+	GLint viewport[4];
+	GLdouble modelview[16];
+	GLdouble projection[16];
+	GLfloat winX, winY, winZ;
+	GLdouble posX, posY, posZ;
 
-//void OnTimer(int value){
-	//****WRITE TIMER CODE HERE
+	glGetDoublev(GL_MODELVIEW_MATRIX, modelview);
+	glGetDoublev(GL_PROJECTION_MATRIX, projection);
+	glGetIntegerv(GL_VIEWPORT, viewport);
+
+	winX = (float)x;
+	winY = (float)viewport[3] - (float)y;
+	glReadPixels(x, int(winY), 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &winZ);
+	gluUnProject(winX, winY, winZ, modelview, projection, viewport, &posX, &posY, &posZ);
+
+	
+
+	
+	for (int i = 0; i < GRID_SIZE; i++) {
+		for (int j = 0;j < GRID_SIZE; j++) {
+			if (gameboard.getTab()[i][j].draged()) {
+				scene.DrawCell(i, j, posX, posY);
+				
+			}
+		}
+	}
+	
+
+}*/
+
+void OnTimer(int value) {
+	
+
+	////Activa movimiento
+	
+	scene.OnTimer(value);
 
 	//sets new timer (do not delete)
-//	glutTimerFunc(25,OnTimer,0);
-//	glutPostRedisplay();
-//}
+	glutTimerFunc(25,OnTimer,0);
+	glutPostRedisplay();
+}
