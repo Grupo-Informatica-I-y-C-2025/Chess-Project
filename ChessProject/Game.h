c865bf878afbde11182ea7e012c50db0aba265d4
Remi-Garcia <remi.garcia@alumnos.upm.es>
2025-04-23 13:27:19 +0200

    Update Game.h
    inclusion de los diferentes nuevos bots

--- ChessProject/Game.h
+++ ChessProject/Game.h
@@ -1,12 +1,14 @@
 #pragma once
 #include "board.h"
-#include "Bot.h"
-
+#include "Bot_V1.h"
+#include "Bot_V2.h"
+#include "Bot_V3.h"
 class Game {
 protected:
 	Move movement;
 	Board* board;
-	Bot machine;
+	Bot_V1 machine1;
+	Bot_V3 machine2;
 	int N;
 	int M;
 
@@ -14,11 +16,13 @@ protected:
 public:
 	bool bot = 1;
 	bool player = 0;
+	bool generated = 0;
 
-	Game(Board* pb) :board(pb), machine(pb, bot) {
+	Game(Board* pb) :board(pb), machine1(pb,bot), machine2(pb) {
 		N = pb->getSizeY();
 		M = pb->getSizeX();		//Grid NxN
-		;
+		movement.sourceSquare = -1;
+		movement.targetSquare = -1;
 
 	}
 
@@ -29,8 +33,9 @@ public:
 
 
 	//public game functions
+	void scanEndGame(bool);
 	bool isgame_over() { return game_over; }
-	void makeMove();
+	void playTurn();
 	
 
 private:
@@ -38,8 +43,4 @@ private:
 
 	bool game_over = false;
 	bool action = false;
-	int src[2] = { -1,-1 }, dest[2] = { -1,-1 };
-
-
-
 };
