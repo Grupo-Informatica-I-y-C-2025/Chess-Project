f2f43350646ee366e80abb2d81fc5eeb3edfd43d
Remi-Garcia <remi.garcia@alumnos.upm.es>
2025-04-25 19:04:16 +0200

    Update board.h
    incluidas en el archivo encabezado de clase las funciones relativas a operaciones de interfaz de Bitboards

--- ChessProject/board.h
+++ ChessProject/board.h
@@ -1,4 +1,3 @@
-
 #ifndef __BOARD_H__
 #define __BOARD_H__
 
@@ -23,6 +22,20 @@
 #define BLACK_KINGSIDE  0x4
 #define BLACK_QUEENSIDE 0x8
 
+struct GameType {
+	bool kramnik;
+	int x;
+	int y;
+};
+const GameType games[6] = {
+		{false, 8, 8},
+		{true, 8, 8},
+		{false, 4, 8} ,
+		{ true, 4, 8 } ,
+		{ false, 4, 5 } ,
+		{ true, 4, 5 }
+};
+
 using namespace std;
 
 enum MoveType {
@@ -39,13 +52,13 @@ enum PieceType {
  ROOK = 3,
  QUEEN = 4,
  KING = 5,
- NONE = 6,
+ EMPTY_CELL = 6,
 };
 enum PieceColor {
 	WHITE = 0,
 	BLACK = 1,
+	NONE = 2,
 };
-
 
 typedef uint64_t Bitboard;
 
@@ -93,9 +106,25 @@ struct MoveResult {
 	bool capturedColor;
 	Bitboard extraState;
 };
-class Board {
 
-public:
+class Game;
+class Bot_V1;
+class Bot_V2;
+class Bot_V3;
+class Bot_V4;
+class Bot_SB;
+class BoardGL;
+
+class Board {
+	friend class Game;
+	friend class Bot_V1;
+	friend class Bot_V2;
+	friend class Bot_V3;
+	friend class Bot_V4;
+	friend class Bot_SB;
+	friend class BoardGL;
+
+protected:
 	// Definiciones de máscaras posicionales
 	static constexpr Bitboard CENTER_SQUARES = 0x0000001818000000ULL;
 	static constexpr Bitboard KING_ZONE_WHITE = 0x0000000000070707ULL;
@@ -143,7 +172,7 @@ public:
 
 	int N;	int M;
 	const bool Kramnik;
-	
+	int gametype;
 	
 
 	ChessState prevStates[500];  // Pila de estados anteriores
@@ -158,10 +187,15 @@ public:
 		currentState.currentAttacks[1] = calculateAttackMap(1);
 	}
 
+	Bitboard boundary;
+	void boundaryMask();
 
 
+
+public:
 	// Constructor principal
-	Board(int n, int m, bool k) : N(n), M(m), Kramnik(k) {
+	Board(int n) : N(games[n].y), M(games[n].x), Kramnik(games[n].kramnik) {
+		gametype = n;
 		initBitboards();
 		setBitboards();
 		initAttackTables();
@@ -176,12 +210,9 @@ public:
 	// Destructor
 	~Board() = default;
 
+	void printBoard(Bitboard piece)const;
 
-	bool isKramnik() const { return Kramnik; }
-
-	void printBoard( Bitboard piece)const;
-	
-
+private:
 
 	//logica basica de generacion de movimientos
 	Bitboard getPieces(bool color) const {
@@ -232,17 +263,16 @@ public:
 	Bitboard calculateAttackMap(bool color) const;
 	Bitboard generateAttacksFrom(int sq, bool ) const;
 	Bitboard generateMovesFrom(int, bool) const;
-	Bitboard getMovementMap(bool);
+	Bitboard getMovementMap(bool)const;
 
 
 	MoveResult makeMove( Move&);
 	void undoMove();
 	MoveType determineMoveType(const Move& move)const;
 
-	bool scanChecks(bool, Bitboard&);
-	bool scanChecks(bool);
+	bool scanChecks(bool, Bitboard&)const;
+	bool scanChecks(bool)const;
 	bool scanCheckMate(bool);
-	bool scanDraw();
 	bool canKingMove(bool);
 	bool canCaptureAttacker(bool, Bitboard);
 	bool canBlockAttacker(bool, Bitboard);
@@ -254,15 +284,9 @@ public:
 
 	vector<Move> generateAllMoves(bool);
 	vector<Move> generateCaptureMoves(bool);
-	vector<Move> generateCheckMoves(bool);
-	/*vector<Move> generateNormalMoves(bool);
-	vector<Move> generateEscapeMoves(bool, Bitboard);
-	vector<Move> generateKingMove(bool);
-	vector<Move> generateCaptureAttacker(bool, Bitboard);
-	vector<Move> generateBlockAttacker(bool, Bitboard);*/
 
 
-
+public:
 	//bitboard to interface
 	void initBitboards();
 	void setBitboards();
@@ -289,10 +313,11 @@ public:
 	bool selected(int x, int y);
 
 	bool BitboardGetTurn() { return currentState.turn; }
-	bool BitboardChangeTurn() { currentState.turn = (currentState.turn) ? 0 : 1; return currentState.turn;}
-
+	bool isKramnik() const { return Kramnik; }
+	int getVariant()const;
 	int getSizeY() { return N; }
 	int getSizeX() { return M; }
+	Bitboard getSelected() { return currentState.selected; }
 
 	//LSB
 	static int portable_ctzll(uint64_t b) {
@@ -323,19 +348,287 @@ public:
 	}
 	
 	// Función auxiliar de desplazamiento seguro
-Bitboard shift(Bitboard b, int dir)const{
-	if (dir > 0) return b << dir;   // Desplazamiento hacia arriba (blancas)
-	else return b >> (-dir);        // Desplazamiento hacia abajo (negras)
-}
+	Bitboard shift(Bitboard b, int dir)const{
+		if (dir > 0) return b << dir;   // Desplazamiento hacia arriba (blancas)
+		else return b >> (-dir);        // Desplazamiento hacia abajo (negras)
+	}
 
-int countBits(Bitboard b) {
-	// Método SWAR (5 operaciones para 64 bits)
-	b = b - ((b >> 1) & 0x5555555555555555);
-	b = (b & 0x3333333333333333) + ((b >> 2) & 0x3333333333333333);
-	return ((b + (b >> 4)) & 0x0F0F0F0F0F0F0F0F) * 0x0101010101010101 >> 56;
-}
+	int countBits(Bitboard b) {
+		// Método SWAR (5 operaciones para 64 bits)
+		b = b - ((b >> 1) & 0x5555555555555555);
+		b = (b & 0x3333333333333333) + ((b >> 2) & 0x3333333333333333);
+		return ((b + (b >> 4)) & 0x0F0F0F0F0F0F0F0F) * 0x0101010101010101 >> 56;
+	}
 };
 
+inline void Board::printBoard(Bitboard board) const {
+	cout << "\n\n";
+	// Imprimir desde la fila superior (i=7) hasta la inferior (i=0)
+	for (int i = 7; i >= 0; i--) {  // <- Cambiar el bucle a decreciente
+		for (int j = 0; j < 8; j++) {
+			// Calcular el bit correspondiente a la casilla (i, j)
+			cout << ((board >> (i * 8 + j)) & 1);
+		}
+		cout << "\n";
+	}
+}
+inline int Board::getVariant()const {
+	int variante = 0;
+	if (gametype == 0 || gametype == 1)variante = 0;
+	if (gametype == 2 || gametype == 3)variante = 1;
+	if (gametype == 4 || gametype == 5)variante = 2;
+	return variante;
+}
+inline void Board::initBitboards() {
+	memset(&currentState, 0, sizeof(ChessState));  // Todos los bits a 0
+}
+inline void Board::setBitboards() {
 
+	initBitboards();
+	currentState.turn = WHITE; // El blanco comienza
+
+
+
+	switch (getVariant()) {
+	case 0:
+		for (int x = 0; x < M; x++) {
+			BitboardSetPiece(x, 6, PAWN, BLACK);
+		}
+		for (int x = 0; x < M; x++) {
+			BitboardSetPiece(x, 1, PAWN, WHITE);
+		}
+		// Torres
+		BitboardSetPiece(0, 7, ROOK, BLACK);    // a8
+		BitboardSetPiece(7, 7, ROOK, BLACK);    // h8
+		BitboardSetPiece(0, 0, ROOK, WHITE);    // a1
+		BitboardSetPiece(7, 0, ROOK, WHITE);    // h1
+		// Alfiles
+		BitboardSetPiece(2, 7, BISHOP, BLACK);  // c8
+		BitboardSetPiece(5, 7, BISHOP, BLACK);  // f8
+		BitboardSetPiece(2, 0, BISHOP, WHITE);  // c1
+		BitboardSetPiece(5, 0, BISHOP, WHITE);  // f1
+		// Caballos
+		BitboardSetPiece(1, 7, KNIGHT, BLACK);  // b8
+		BitboardSetPiece(6, 7, KNIGHT, BLACK);  // g8
+		BitboardSetPiece(1, 0, KNIGHT, WHITE);  // b1
+		BitboardSetPiece(6, 0, KNIGHT, WHITE);  // g1
+		// Reinas
+		BitboardSetPiece(3, 7, QUEEN, BLACK);   // d8
+		BitboardSetPiece(3, 0, QUEEN, WHITE);   // d1
+		// Reyes
+		BitboardSetPiece(4, 7, KING, BLACK);    // e8
+		BitboardSetPiece(4, 0, KING, WHITE);    // e1
+		currentState.castlingRights = WHITE_KINGSIDE | WHITE_QUEENSIDE | BLACK_KINGSIDE | BLACK_QUEENSIDE;
+		break;
+	case 1:
+		for (int x = 0; x < M; x++) {
+			BitboardSetPiece(x, 6, PAWN, BLACK);
+		}
+		for (int x = 0; x < M; x++) {
+			BitboardSetPiece(x, 1, PAWN, WHITE);
+		}
+		// Torres
+		BitboardSetPiece(3, 7, ROOK, BLACK);
+		BitboardSetPiece(3, 0, ROOK, WHITE);
+		// Alfiles
+		BitboardSetPiece(1, 7, BISHOP, BLACK);
+		BitboardSetPiece(1, 0, BISHOP, WHITE);
+		// Caballos
+		BitboardSetPiece(2, 7, KNIGHT, BLACK);
+		BitboardSetPiece(2, 0, KNIGHT, WHITE);
+		// Reyes
+		BitboardSetPiece(0, 7, KING, BLACK);
+		BitboardSetPiece(0, 0, KING, WHITE);
+		break;
+	case 2:
+		for (int x = 0; x < M; x++) {
+			BitboardSetPiece(x, 3, PAWN, BLACK);
+		}
+		for (int x = 0; x < M; x++) {
+			BitboardSetPiece(x, 1, PAWN, WHITE);
+		}
+		// Reinas
+		BitboardSetPiece(1, 4, QUEEN, BLACK);
+		BitboardSetPiece(1, 0, QUEEN, WHITE);
+		// Torres
+		BitboardSetPiece(3, 4, ROOK, BLACK);
+		BitboardSetPiece(3, 0, ROOK, WHITE);
+		BitboardSetPiece(0, 4, ROOK, BLACK);
+		BitboardSetPiece(0, 0, ROOK, WHITE);
+		// Reyes
+		BitboardSetPiece(2, 4, KING, BLACK);
+		BitboardSetPiece(2, 0, KING, WHITE);
+
+		break;
+	}
+
+}
+
+inline int Board::coordToPos(int x, int y) const {
+	return  (y * 8 + x); // Ej: (2,3) ? 1 << 26
+}
+inline Bitboard Board::coordToBit(int x, int y) const {
+	return 1ULL << (y * 8 + x); // Ej: (2,3) ? 1 << 26
+}
+inline void Board::bitToCoord(Bitboard b, int& x, int& y) const {
+	int pos = portable_ctzll(b); // Posición del bit activo
+	x = pos % 8;
+	y = pos / 8;
+}
+inline void Board::removePiece(int x, int y) {
+	Bitboard mask = coordToBit(x, y);
+
+	// Buscar en todos los tipos y colores
+	for (int color = 0; color < 2; color++) {
+		for (int type = 0; type < 6; type++) { // Saltar EMPTY_CELL
+			if (currentState.pieces[color][type] & mask) {
+				currentState.pieces[color][type] &= ~mask;
+				currentState.occupancy &= ~mask;
+				return;
+			}
+		}
+	}
+}
+inline void Board::removeFlag(int x, int y) {
+	Bitboard mask = coordToBit(x, y);
+
+	// Buscar en todos los tipos y colores
+	if (currentState.enPassant & mask) {
+		currentState.enPassant &= ~mask;
+		currentState.occupancy &= ~mask;
+		return;
+	}
+}
+
+
+inline int Board::BitboardGetColor(int x, int y) const {
+	Bitboard mask = coordToBit(x, y);
+	int color = 2;
+
+	for (int c = 0; c < 2; c++) {
+		for (int t = 0; t < 6; t++) { // EMPTY_CELL no se almacena
+			if (currentState.pieces[c][t] & mask) {
+				return c;
+			}
+		}
+	}return color;
+}
+inline int Board::BitboardGetColor(int sq) const {
+	Bitboard mask = 1ULL << sq;
+	int color = 2;
+
+	for (int c = 0; c < 2; c++) {
+		for (int t = 0; t < 6; t++) { // EMPTY_CELL no se almacena
+			if (currentState.pieces[c][t] & mask) {
+				return c;
+			}
+		}
+	}return color;
+}
+inline int Board::BitboardGetColor(int sq, ChessState state) const {
+	Bitboard mask = 1ULL << sq;
+	int color = 2;
+
+	for (int c = 0; c < 2; c++) {
+		for (int t = 0; t < 6; t++) { // EMPTY_CELL no se almacena
+			if (state.pieces[c][t] & mask) {
+				return c;
+			}
+		}
+	}return color;
+}
+inline int Board::BitboardGetType(int x, int y) const {
+	Bitboard mask = coordToBit(x, y);
+	int type = 6;
+
+	for (int c = 0; c < 2; ++c) {
+		for (int t = 0; t < 6; ++t) { // EMPTY_CELL no se almacena
+			if (currentState.pieces[c][t] & mask) {
+				return t;
+			}
+		}
+	}return type;
+}
+inline int Board::BitboardGetType(int sq) const {
+	Bitboard mask = 1ULL << sq;
+	int type = 6;
+
+	for (int c = 0; c < 2; ++c) {
+		for (int t = 0; t < 6; ++t) { // EMPTY_CELL no se almacena
+			if (currentState.pieces[c][t] & mask) {
+				return t;
+			}
+		}
+	}return type;
+}
+inline void Board::BitboardSetPiece(int x, int y, int pieceType, bool color) {
+	Bitboard mask = coordToBit(x, y);
+
+	// Limpiar cualquier pieza existente
+	removePiece(x, y);
+
+	// Actualizar bitboards específicos
+	currentState.pieces[color][pieceType] |= mask;
+	currentState.occupancy |= mask;
+}
+
+
+inline void Board::BitboardSetFlag(int x, int y) {
+	Bitboard mask = coordToBit(x, y);
+
+	// Limpiar cualquier flag
+	removeFlag(x, y);
+
+	// Actualizar bitboards específicos
+	currentState.enPassant |= mask;
+
+}
+inline int Board::BitboardGetFlag(int x, int y) {
+	Bitboard mask = coordToBit(x, y);
+	int flag = 0;
+
+	if (currentState.enPassant & mask) flag = 1;
+
+	return flag;
+}
+
+inline void Board::selectCell(int x, int y) {
+	Bitboard mask = coordToBit(x, y);
+
+	// Limpiar cualquier flag
+	unselectCell(x, y);
+
+	// Actualizar bitboards específicos
+	currentState.selected |= mask;
+
+}
+inline void Board::unselectCell(int x, int y) {
+	Bitboard mask = coordToBit(x, y);
+
+	// Buscar en todos los tipos y colores
+	if (currentState.selected & mask) {
+		currentState.selected &= ~mask;
+
+	}
+}
+inline bool Board::selected(int x, int y) {
+	Bitboard mask = coordToBit(x, y);
+
+	bool s = 0;
+	if (currentState.selected & mask)s = 1;
+	return s;
+}
+
+inline void Board::cleanEnpassant() {
+
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < N; j++) {
+
+			removeFlag(i, j);
+		}
+	}
+
+
+}
 
 #endif
