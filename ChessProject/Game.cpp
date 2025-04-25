94f5a1345b2a98c635f3bbabf6135cd1a7fb0e1e
Remi-Garcia <remi.garcia@alumnos.upm.es>
2025-04-25 19:02:27 +0200

    Update Game.cpp

--- ChessProject/Game.cpp
+++ ChessProject/Game.cpp
@@ -3,8 +3,8 @@
 
 //mover a boardGL
 void Game::unselectAll() {
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < N; j++) {
 			board->unselectCell(i,j);
 		}
 	}
@@ -12,7 +12,7 @@ void Game::unselectAll() {
 void Game::activate( int xcell_sel, int ycell_sel)
 {
 	////Activar casillas -- parte propia	
-	if ((xcell_sel >= 0 && xcell_sel < N) && (ycell_sel >= 0 && ycell_sel < N)) {
+	if ((xcell_sel >= 0 && xcell_sel < N) && (ycell_sel >= 0 && ycell_sel < M)) {
 		if (board->selected(ycell_sel, xcell_sel))
 		{
 			board->unselectCell(ycell_sel, xcell_sel);
@@ -23,7 +23,7 @@ void Game::activate( int xcell_sel, int ycell_sel)
 			unselectAll();
 			board->selectCell(ycell_sel, xcell_sel);
 			//board->printBoard(board->generateMovesFrom(board->coordToPos(ycell_sel, xcell_sel),board->BitboardGetColor(ycell_sel, xcell_sel)));
-			movement.sourceSquare = -1;
+			if(!board->isKramnik())movement.sourceSquare = -1;
 			registerCall(xcell_sel, ycell_sel);
 		}
 		else if (board->BitboardGetColor(ycell_sel, xcell_sel) == !board->BitboardGetTurn() || board->BitboardGetColor(ycell_sel, xcell_sel) == 2) {
@@ -34,7 +34,7 @@ void Game::activate( int xcell_sel, int ycell_sel)
 
 }
 void Game::registerCall(int xcell_sel, int ycell_sel) {
-	if ((xcell_sel >= 0 && xcell_sel < N) && (ycell_sel >= 0 && ycell_sel < N)) {
+	if ((xcell_sel >= 0 && xcell_sel < N) && (ycell_sel >= 0 && ycell_sel < M)) {
 		if (movement.sourceSquare<0)
 		{
 			movement.sourceSquare = xcell_sel * 8 + ycell_sel;
@@ -50,16 +50,15 @@ void Game::playTurn() {
 	bool turn = board->BitboardGetTurn();
 
 	if (!game_over && turn == bot && !generated) {
-		movement = machine1.botMove(turn);
-		//cout << "\nmove :" << movement.sourceSquare % 8 << movement.sourceSquare / 8 << " " << movement.targetSquare % 8 << movement.targetSquare / 8;
-		//if (!board->isLegalmove(movement))cout << "\nmove is ilegal";
+		movement = black_bot.botMove(turn);
+		cout << "\nmove :" << movement.sourceSquare % 8 << movement.sourceSquare / 8 << " " << movement.targetSquare % 8 << movement.targetSquare / 8;
 		generated = true;
 	}
-	if (!game_over && turn == player && !generated) {
-		movement = machine2.botMove(turn);
+	/*if (!game_over && turn == player && !generated) {
+		movement = white_bot.botMove(turn);
 		//cout << "\nmove :" << movement.sourceSquare % 8 << movement.sourceSquare / 8 << " " << movement.targetSquare % 8 << movement.targetSquare / 8;
 		generated = true;
-	}
+	}*/
 
 	if (movement.sourceSquare >= 0 && movement.targetSquare >= 0) {
 		movement.pieceType = board->BitboardGetType(movement.sourceSquare);
@@ -69,6 +68,9 @@ void Game::playTurn() {
 			scanEndGame(!turn);
 			if (!game_over && board->scanChecks(!turn)) cout << "\ncheck";
 		}
+		else {
+			cout << "\nIllegal move";
+		}
 		//reset
 		
 		movement.sourceSquare = -1, movement.targetSquare = -1;
@@ -77,16 +79,18 @@ void Game::playTurn() {
 
 
 void Game::scanEndGame(bool color) {
-
-	if (board->scanCheckMate(color)) {
-		cout << "\ncheckmate";
-		game_over = true;
-		return;
-	}
-	if (board->scanDraw()) {
-		cout << "\ndraw";
-		game_over = true;
-		return;
+	vector<Move> moves = board->generateAllMoves(color);
+	if (moves.size() == 0) {
+		if (board->scanChecks(color)) {
+			cout << "\ncheckmate";
+			game_over = true;
+			return;
+		}
+		else {
+			cout << "\ndraw";
+			game_over = true;
+			return;
+		}
 	}
 	if (board->moveCount == 499) {
 		game_over = true;
